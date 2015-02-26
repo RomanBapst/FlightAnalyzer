9a545c69e7d0d1e68406de7540153ce497c9f5dd
tumbili <bapstr@ethz.ch>
2015-02-26 20:43:01 +0100

    plot desired attitude

--- plot_flight_maneuver.py
+++ plot_flight_maneuver.py
@@ -29,12 +29,16 @@ class FlightData(object):
         self.qx = []
         self.qy = []
         self.qz = []
+        self.qw_des = []
+        self.qx_des = []
+        self.qy_des = []
+        self.qz_des = []
         self.read_data(self.csv_file_name)
 
         # change look of the plane here
         self.x_coord = np.array([0,0,0,0,0,0,-0.2,-0.2,0])
         self.y_coord = np.array([0,0.5,-0.5,0,0,0,0,0,0])
-        self.z_coord = np.array([0.5,-0.5,-0.5,0.5,0.7,0,-0.1,-0.2,-0.2])
+        self.z_coord = -np.array([0.5,-0.5,-0.5,0.5,0.7,0,-0.1,-0.2,-0.2])
         
         self.INDEX = np.arange(0,len(self.time),10)
         self.sim_len = len(self.INDEX)
@@ -60,6 +64,12 @@ class FlightData(object):
             self.qx.append(float(data[header_dic["ATT_qx"]]))
             self.qy.append(float(data[header_dic["ATT_qy"]]))
             self.qz.append(float(data[header_dic["ATT_qz"]]))
+            
+            self.qw_des.append(float(data[header_dic["ATSP_qw"]]))
+            self.qx_des.append(float(data[header_dic["ATSP_qx"]]))
+            self.qy_des.append(float(data[header_dic["ATSP_qy"]]))
+            self.qz_des.append(float(data[header_dic["ATSP_qz"]]))
+            
     
     def quat_to_rot(self,q):
         #compute rotation matrix from quaternion
@@ -70,7 +80,7 @@ class FlightData(object):
     
         R = np.zeros((3,3))
          
-         R[0,0] = q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3
+        R[0,0] = q0 * q0 + q1 * q1 - q2 * q2 - q3 * q3
         R[0,1] = 2 * q1 * q2 - 2 * q0 * q3
         R[0,2] = 2 * q1 * q3 + 2 * q0 * q2
         R[1,0] = 2 * q1 * q2 + 2 * q0 * q3
@@ -91,21 +101,36 @@ class FlightData(object):
         ax.set_ylim3d([self.y[self.INDEX[self.frame]]-1.0, 1.0+self.y[self.INDEX[self.frame]]])
         ax.set_ylabel('Y')
         ax.set_zlim3d([-self.z[self.INDEX[self.frame]]-1.0, - self.z[self.INDEX[self.frame]]+1.0])
+        ax.invert_zaxis()
         x = []
         y = []
         z = []
         line = ax.plot(self.x_coord, self.y_coord, self.z_coord)[0]
-	q = [self.qw[self.INDEX[self.frame]],self.qx[self.INDEX[self.frame]],self.qy[self.INDEX[self.frame]],self.qz[self.INDEX[self.frame]]]
+        q = [self.qw[self.INDEX[self.frame]],self.qx[self.INDEX[self.frame]],self.qy[self.INDEX[self.frame]],self.qz[self.INDEX[self.frame]]]
         R = self.quat_to_rot(q)
         for index,item in enumerate(self.x_coord):
             vec = np.dot(R,[self.x_coord[index],self.y_coord[index],self.z_coord[index]])
             x.append(vec[0] + self.x[self.INDEX[self.frame]])
             y.append(vec[1] + self.y[self.INDEX[self.frame]])
             z.append(vec[2] - self.z[self.INDEX[self.frame]])
-            
+        
+        x_des = []
+        y_des = []
+        z_des = []
+        line1 = ax.plot(self.x_coord, self.y_coord, self.z_coord)[0]
+        q_des = [self.qw_des[self.INDEX[self.frame]],self.qx_des[self.INDEX[self.frame]],self.qy_des[self.INDEX[self.frame]],self.qz_des[self.INDEX[self.frame]]]
+        R_des = self.quat_to_rot(q_des)        
+        for index,item in enumerate(self.x_coord):
+            vec = np.dot(R_des,[self.x_coord[index],self.y_coord[index],self.z_coord[index]])
+            x_des.append(vec[0] + self.x[self.INDEX[self.frame]])
+            y_des.append(vec[1] + self.y[self.INDEX[self.frame]])
+            z_des.append(vec[2] - self.z[self.INDEX[self.frame]])
+        
         line.set_data(np.array(x), np.array(y))
         line.set_3d_properties(np.array(z))
-        return line
+        line1.set_data(np.array(x_des), np.array(y_des))
+        line1.set_3d_properties(np.array(z_des))
+        return [line,line1]
                    
     def user_input(self):
         print "type 'help' for help"
@@ -138,7 +163,7 @@ class FlightData(object):
         
 def _main():
     file_name = sys.argv[1]
-    sys.argv = [file_name,file_name,'-f',file_name.split('.')[0]+'.csv','-t','TIME','-m','TIME','-m','ATT','-m','LPOS' ]
+    sys.argv = [file_name,file_name,'-f',file_name.split('.')[0]+'.csv','-t','TIME','-m','TIME','-m','ATT','-m','LPOS','-m','ATSP' ]
     sdlog2_dump._main()
 
     x = FlightData(file_name)
@@ -155,8 +180,10 @@ def _main():
         ax.set_xlabel('Y')
         ax.set_zlim3d([-1.0, 1.0])
         ax.set_xlabel('Z')
-        
+        lines = []
         line = ax.plot([-1,0,1],[-1,0,1],[-1,0,1])[0]
+        line1 = ax.plot([-1,0,1],[-1,0,1],[-1,0,1])[0]
+        lines = [line,line1]
         line_ani = animation.FuncAnimation(fig, x.animate,blit=False,interval=10)
         plt.show()
                                       
